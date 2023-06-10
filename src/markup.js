b27438d88e4625bfa1168937feb655d183d66297
Alexeencokirill <97600028+Alexeencokirill@users.noreply.github.com>
2023-06-11 00:47:33 +0200

    update-03

--- src/markup.js
+++ src/markup.js
@@ -4,15 +4,19 @@ export function markupGallery(array, gallery) {
         return `<div class="photo-card"> <a class="gallery__link" href="${elem.largeImageURL}" >
     <img class="gallery__image" src="${elem.webformatURL}" alt="${elem.tags}" loading="lazy" /></a>
     <div class="info">
+
       <p class="info-item">
         <b>Likes:</b> ${elem.likes}
       </p>
+
       <p class="info-item">
         <b>Views:</b> ${elem.views}
       </p>
+
       <p class="info-item">
         <b>Comments:</b> ${elem.comments}
       </p>
+      
       <p class="info-item">
         <b>Downloads:</b> ${elem.downloads}
       </p>
---
+++ src/searchImage.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const URL = 'https://pixabay.com/api/';
+const KEY = '37189607-24fd4809c428b985013c85be9';
+
+export async function searchImage(inputValue, page) {
+    const response = await axios.get(
+        `${URL}?key=${KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
+    return response;
+}
