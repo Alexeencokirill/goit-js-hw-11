ed459ae445fa96f5ae10c2d710942abbe4245535
Alexeencokirill <97600028+Alexeencokirill@users.noreply.github.com>
2023-06-11 22:38:56 +0200

    update-05

--- src/searchImagesHandler.js
+++ src/searchImagesHandler.js
@@ -1,20 +1,18 @@
 import { searchImages } from './searchImages';
-// import { Loading } from 'notiflix/build/notiflix-loading-aio';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import { Notify } from 'notiflix';
-import Notiflix from 'notiflix';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 export async function searchImagesHandler(inputValue, page) {
   try {
-    Notiflix.Loading.standard('Loading...');
+    Loading.standard('Loading...');
 
     const response = await searchImages(inputValue, page);
 
     return response.data;
   } catch (error) {
-    // Notiflix.Notify.error('Download error, try again later');
+    Notify.error('Download error, try again later');
     console.log(error);
   } finally {
-    Notiflix.Loading.remove();
+    Loading.remove();
   }
 }
